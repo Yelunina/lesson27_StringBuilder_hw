5b5febd4897908fa714b7549830f93f5bd407edf
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-10-13 20:16:44 +0200

    code

--- src/ait/stringbuilder/StringReverse.java
+++ src/ait/stringbuilder/StringReverse.java
@@ -4,11 +4,29 @@ public class StringReverse {
     public static void main(String[] args) {
         String reverse = reversOrder("Ukraine");
         System.out.println(reverse);
+        reverse = reversOrder1("Ukraine");
+        System.out.println(reverse);
+        reverse = reversOrder2("Ukraine");
+        System.out.println(reverse);
     }
 
     public static String reversOrder(String str) {
-        String reverse = new StringBuffer(str).reverse().toString();
+        StringBuilder builder = new StringBuilder(str);
+        builder.reverse();
+        String reverse = builder.toString();
         return reverse;
     }
 
+    public static String reversOrder1(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        return reverse;
+    }
+
+    public static String reversOrder2(String str) {
+        String reverse = new StringBuilder(str).reverse().toString();
+        return reverse;
+    }
 }
